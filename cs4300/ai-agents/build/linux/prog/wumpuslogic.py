0593c14dfc50cd91d116ff4aeec5bef041698abe
ebeilmann <ebeilman@dmail.dixie.edu>
2017-01-06 01:38:16 -0700

    Add files via upload

---
+++ cs4300/ai-agents/build/linux/prog/wumpuslogic.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+main():
+	print "hi"
+#	x=4
+#	y=4
+#	p=pit
+#	b=breeze
+#	w=wumpus
+#	s=stench
+#	g=glitter
+
+	#p or b 
+	#w or s
+	#g or !g
+
+#	for i in range(0, x):
+#		for j in range(0, y):
+#			print i,j
+
+main()
