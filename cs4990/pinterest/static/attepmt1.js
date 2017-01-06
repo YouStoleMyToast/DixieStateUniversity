8bf7b17fccc4fec6c7eed0801256524f52c26c63
ebeilmann <ebeilman@dmail.dixie.edu>
2017-01-06 03:13:35 -0700

    Add files via upload

---
+++ cs4990/pinterest/static/attepmt1.js
@@ -0,0 +1,32 @@
+var current_page = 1; // the page number I'm starting with
+var max_page = 5;
+
+function update_page_with_new_items(html) {
+    $('#pin-list-head').append(html);
+    $('#pin-list-head').masonry();
+}
+
+$(window).scroll(function(){
+    if($(window).scrollTop() + $(window).height() >= $(document).height() - 500)
+    {
+       var next = current_page + 1; 
+       var data = new Array();
+       var lock = null;
+       if (next <= max_page && !lock) {
+           data.push({name: "page", value: next});
+           lock = $.ajax({
+                    url: ".",
+                    data: data,
+                    type: 'GET',
+                    success: function(html) {
+                        update_page_with_new_items(html);
+                        lock = null;
+                        current_page++;
+                    },
+                    error: function(data) {
+                        alert('Problem happened.');
+                    },
+               });
+       }
+    }
+});
