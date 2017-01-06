c8cc0d547fbd9d79ab88fd278d0c745d041a37ca
ebeilmann <ebeilman@dmail.dixie.edu>
2017-01-06 03:12:59 -0700

    Add files via upload

---
+++ cs4990/pinterest/pinterest/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^users', RedirectView.as_view(url='/')),
+    url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+	url(r'^',include('jigsaw.urls', namespace='jigsaw')),
+)+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
